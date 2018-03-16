7edcbd1e838108b3bfe040cc0f66c2cdb3bbd53e
heybluguy <heybluguy@yahoo.com>
2018-03-15 21:30:22 -0600

    Add Orders and orderitems.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'orders/index'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get 'orders/show'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
